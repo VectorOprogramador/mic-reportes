5e5f1968266bd9371e7c258eee7520d9b6c3c5b4
Diego Améstica <di.amestica@duocuc.cl>
2025-05-14 14:32:05 -0400

    git sincronizado

---
+++ src/main/java/com/perfulandia/mic_reportes/MicReportesApplication.java
@@ -0,0 +1,13 @@
+package com.perfulandia.mic_reportes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicReportesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicReportesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perfulandia/mic_reportes/MicReportesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfulandia.mic_reportes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicReportesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
